5fff56758296f1e936e2fe27274276008ca0a01e
isakleong <37265470+isakleong@users.noreply.github.com>
2020-07-28 06:45:06 +0700

    Upload project to git

---
+++ Project PBO Space Invader/Bullet.h
@@ -0,0 +1,31 @@
+//#include <SFML\Graphics.hpp>
+//#include <iostream>
+//using namespace std;
+//using namespace sf;
+//
+//class Bullet{
+//private:
+//	Sprite bullet;
+//public :
+//	void fire(int speed){
+//		bullet.move(0,-speed);
+//	}
+//	int getR(){
+//		return bullet.getPosition().x;
+//	}
+//	int getL(){
+//		return bullet.getPosition().x;
+//	}
+//	int getT(){
+//		return bullet.getPosition().y;
+//	}
+//	int getB(){
+//		return bullet.getPosition().y;
+//	}
+//	void draw(RenderWindow &window){
+//		Texture txture5;
+//		txture5.loadFromFile("Projectile.png");
+//		bullet.setTexture(txture5);
+//		window.draw(bullet);
+//	}
+//};
